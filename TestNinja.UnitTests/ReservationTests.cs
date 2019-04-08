e83dad9c6fdac6d19b8f8c13bac8510a328c340a
lubelle <belleliullh@gmail.com>
2019-04-08 11:49:19 -0400

    add test project to solution

---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_AdminCancelling_ReturnsTrue()
+        {
+            var reservation = new Reservation() { };
+
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            Assert.IsTrue(result);
+        }
+    }
+}
